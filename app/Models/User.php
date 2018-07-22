790672bb04b76b1368323641ac4ba51006468159
Ricky Delorier <ricky.delorier@agencymatrix.com>
2018-07-22 19:42:15 -0400

    Add messages package to act as prayers

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,13 +1,14 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
+use Cmgmyr\Messenger\Traits\Messagable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, Messagable;
 
     /**
      * The attributes that are mass assignable.
