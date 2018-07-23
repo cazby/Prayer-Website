57d96b413796ff9f5b44b5622796ad1d93b76797
Ricky Delorier <ricky.delorier@agencymatrix.com>
2018-07-22 20:01:58 -0400

    Rename migration for consistency

--- database/migrations/2014_12_04_124531_add_soft_deletes_to_threads_table.php
+++ database/migrations/2014_12_04_124531_add_soft_deletes_to_threads_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddSoftdeletesToThreadsTable extends Migration
+class AddSoftDeletesToThreadsTable extends Migration
 {
     /**
      * Run the migrations.
