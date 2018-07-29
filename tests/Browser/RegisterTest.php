eec214b59e2892bf972100d80ad0463136f4aa14
Ricky Delorier <ricky.delorier@agencymatrix.com>
2018-07-29 11:09:01 -0400

    Test email confirmation flow

--- tests/Browser/RegisterTest.php
+++ tests/Browser/RegisterTest.php
@@ -5,6 +5,7 @@ namespace Tests\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
+use App\Models\User;
 
 class RegisterTest extends DuskTestCase
 {
@@ -13,7 +14,7 @@ class RegisterTest extends DuskTestCase
     /**
      * A Dusk test example.
      */
-    public function testExample()
+    public function test_user_must_confirm_email_to_login()
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/register')
@@ -26,6 +27,25 @@ class RegisterTest extends DuskTestCase
                 ->press('Register')
                 ->assertPathIs('/login')
                 ->assertSee(__('confirmation::confirmation.confirmation_info'));
+
+            $browser->visit('/login')
+                ->type('email', 'rdelorier@gmail.com')
+                ->type('password', 'secret')
+                ->press('Login')
+                ->assertPathIs('/login')
+                ->assertSourceHas(__('confirmation::confirmation.not_confirmed', [
+                    'resend_link' => route('auth.resend_confirmation')
+                ]));
+
+            $browser->visit("register/confirm/" . User::value('confirmation_code'))
+                ->assertPathIs('/login')
+                ->assertSee(__('confirmation::confirmation.confirmation_successful'));
+
+            $browser->visit('/login')
+                ->type('email', 'rdelorier@gmail.com')
+                ->type('password', 'secret')
+                ->press('Login')
+                ->assertPathIs('/home');
         });
     }
 }
